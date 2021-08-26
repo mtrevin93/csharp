2f6c603f8087e70abce02770ca4c5a1b7dca53b0
Michael Trevino <mtrevin93@gmail.com>
2021-08-26 10:37:40 -0400

    game prompts user for adventurer name

--- exercises/Quest/Program.cs
+++ exercises/Quest/Program.cs
@@ -43,7 +43,9 @@ namespace Quest
             int maxAwesomeness = 100;
 
             // Make a new "Adventurer" object using the "Adventurer" class
-            Adventurer theAdventurer = new Adventurer("Jack");
+            Console.WriteLine("Greetings, worthy adventurer...Err, what was your name, again?");
+            var adventurerName = Console.ReadLine();
+            Adventurer theAdventurer = new Adventurer(adventurerName);
 
             // A list of challenges for the Adventurer to complete
             // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
